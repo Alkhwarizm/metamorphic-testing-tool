dd7ae5d78e0858b34811fc8bb637fc30d0792c64
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-04-06 10:26:12 +0700

    Added one more relation to api-test.

--- api-test.js
+++ api-test.js
@@ -7,6 +7,7 @@ const {
 } = require('./index.js');
 
 const ACCESS_TOKEN = 'Bearer d6147d8858b12f4a60c1ebc4e4aaff4aea6c465a88e0d5fe29d0142479ccb409';
+const comparator = (a, b) => a - b;
 
 const address = {
     uri: 'https://kitsu.io/api/edge/categories',
@@ -56,7 +57,6 @@ const tFn1 = (parameters) => {
     return [source, following]
 }
 const rFn1 = (outputs) => {
-    const comparator = (a, b) => a - b;
     return outputs[0].sort(comparator).join('') === outputs[1].sort(comparator).join('');
 }
 const mr1 = new MetamorphicRelation(tFn1, rFn1, 'Should return same items even when sorted');
@@ -77,8 +77,27 @@ const rFn2 = (outputs) => {
     if (outputs[1].length === 0) throw new Error('empty following output');
     return outputs[0].every(el => !outputs[1].includes(el));
 }
-const mr2 = new MetamorphicRelation(tFn2, rFn2, 'Should return exactly different items')
+const mr2 = new MetamorphicRelation(tFn2, rFn2, 'Should return exactly different items');
 
-const test = new MetamorphicTesting(api, [mr2]);
+const tFn3 = (parameters) => {
+    const source = {
+        'page[limit]': 300,
+    }
+    const following1 = {
+        'page[limit]': source["page[limit]"],
+        'filter[nsfw]': false,
+    }
+    const following2 = {
+        'page[limit]': source["page[limit]"],
+        'filter[nsfw]': true,
+    }
+    return [source, following1, following2];
+}
+const rFn3 = (outputs) => {
+    return outputs[0].sort(comparator).join('') === outputs[1].concat(outputs[2]).sort(comparator).join('');
+}
+const mr3 = new MetamorphicRelation(tFn3, rFn3, 'Union of the following outputs should equal the source output')
+
+const test = new MetamorphicTesting(api, [mr1, mr2, mr3]);
 
 TestExecutor.execute(test);
