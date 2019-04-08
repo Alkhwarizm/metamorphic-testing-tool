1e73b8c28cf2b1fd22e26b6563a0ba5181bd7d08
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-04-08 08:17:04 +0700

    Rewrote api-test sample.

--- samples/api-test.js
+++ samples/api-test.js
@@ -1,18 +1,10 @@
-const { 
-    MetamorphicTesting, 
-    MetamorphicRelation, 
-    APIUnderTest,
-    TestExecutor,
-    helper
-} = require('../index.js');
+const MetamorphicTest = require('../index.js');
+const helper = MetamorphicTest.helper;
+const test = new MetamorphicTest();
 
+const TEST_URI = 'https://kitsu.io/api/edge/categories'
 const ACCESS_TOKEN = 'Bearer d6147d8858b12f4a60c1ebc4e4aaff4aea6c465a88e0d5fe29d0142479ccb409';
 const comparator = (a, b) => a - b;
-
-const address = {
-    uri: 'https://kitsu.io/api/edge/categories',
-    method: 'GET',
-};
 
 const parameters = {
     'page[limit]': 10,
@@ -21,7 +13,9 @@ const parameters = {
     'filter[nsfw]': null,
 }
 
-const wrapper = (input) => {
+test.defineAPI(TEST_URI, 'GET', parameters);
+
+test.setWrapper((input) => {
     // input is like parameters with specific values for each properties
     return {
         qs: input,
@@ -33,71 +27,63 @@ const wrapper = (input) => {
         json: true
     }
     // it return request-promise options object
-}
+});
 
-const extractor = (resp) => {
+test.setExtractor((resp) => {
     // resp is promise returned by request-promise
     return resp.then((jsonResp) => {
         return jsonResp.data.map(item => item.id); // only take the id
     });
     // it return object or list that will be compared by metamorphic relation
-}
+});
 
-const api = new APIUnderTest(address, parameters, {wrapper, extractor});
+test.addRelation('Should return the same items even when sorted.',
+    (parameters) => {
+        const source = {
+            'page[limit]': 300,
+        }
+        const following = {
+            'page[limit]': source["page[limit]"],
+            'sort': helper.chooseRandomElement(['title', 'totalMediaCount'])
+        }
+        return [source, following]
+    }, (outputs) => {
+        return outputs[0].sort(comparator).join('') === outputs[1].sort(comparator).join('');
+    });
 
+test.addRelation('Should return exactly different items', 
+    (parameters) => {
+        const source = {
+            'page[limit]': 300,
+            'filter[nsfw]': false,
+        }
+        const following = {
+            'page[limit]': source["page[limit]"],
+            'filter[nsfw]': true,
+        }
+        return [source, following]
+    }, (outputs) => {
+        if (outputs[0].length === 0) throw new Error('empty source output');
+        if (outputs[1].length === 0) throw new Error('empty following output');
+        return outputs[0].every(el => !outputs[1].includes(el));
+    });
 
-const tFn1 = (parameters) => {
-    const source = {
-        'page[limit]': 300,
-    }
-    const following = {
-        'page[limit]': source["page[limit]"],
-        'sort': helper.chooseRandomElement(['title', 'totalMediaCount'])
-    }
-    return [source, following]
-}
-const rFn1 = (outputs) => {
-    return outputs[0].sort(comparator).join('') === outputs[1].sort(comparator).join('');
-}
-const mr1 = new MetamorphicRelation(tFn1, rFn1, 'Should return same items even when sorted');
+test.addRelation('Union of the following outputs should equal the source output',
+    (parameters) => {
+        const source = {
+            'page[limit]': 300,
+        }
+        const following1 = {
+            'page[limit]': source["page[limit]"],
+            'filter[nsfw]': false,
+        }
+        const following2 = {
+            'page[limit]': source["page[limit]"],
+            'filter[nsfw]': true,
+        }
+        return [source, following1, following2];
+    }, (outputs) => {
+        return outputs[0].sort(comparator).join('') === outputs[1].concat(outputs[2]).sort(comparator).join('');
+    })
 
-const tFn2 = (parameters) => {
-    const source = {
-        'page[limit]': 300,
-        'filter[nsfw]': false,
-    }
-    const following = {
-        'page[limit]': source["page[limit]"],
-        'filter[nsfw]': true,
-    }
-    return [source, following]
-}
-const rFn2 = (outputs) => {
-    if (outputs[0].length === 0) throw new Error('empty source output');
-    if (outputs[1].length === 0) throw new Error('empty following output');
-    return outputs[0].every(el => !outputs[1].includes(el));
-}
-const mr2 = new MetamorphicRelation(tFn2, rFn2, 'Should return exactly different items');
-
-const tFn3 = (parameters) => {
-    const source = {
-        'page[limit]': 300,
-    }
-    const following1 = {
-        'page[limit]': source["page[limit]"],
-        'filter[nsfw]': false,
-    }
-    const following2 = {
-        'page[limit]': source["page[limit]"],
-        'filter[nsfw]': true,
-    }
-    return [source, following1, following2];
-}
-const rFn3 = (outputs) => {
-    return outputs[0].sort(comparator).join('') === outputs[1].concat(outputs[2]).sort(comparator).join('');
-}
-const mr3 = new MetamorphicRelation(tFn3, rFn3, 'Union of the following outputs should equal the source output')
-
-const test = new MetamorphicTesting(api, [mr1, mr2, mr3]);
-
-TestExecutor.execute(test);
+test.execute();
