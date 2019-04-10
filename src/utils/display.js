7fff06f77670e1bc6686f6ee4550a0f72158227b
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-04-10 09:15:48 +0700

    Refactored column setting.

--- src/utils/display.js
+++ src/utils/display.js
@@ -9,29 +9,50 @@ function showTitle() {
 function createResultSummaryTable(contents) {
     const ui = createUI();
     const headerColor = 'bold.inverse.yellow';
+    const footerColor = 'bold.yellow';
     const rowColor = (failed) => failed ? 'red' : 'green';
-    const defaultColSetting = { width: 12 }
-    const firstColSetting = { width: 6, padding: [0, 1, 0, 0], align: 'right'  }
-    const thirdColSetting = { width: 12, padding: [0, 1, 0, 0], align: 'right'  }
+    const columnSetting = [
+        { width: 6, padding: [0, 1, 0, 0], align: 'right'  },
+        {},
+        { width: 12, padding: [0, 1, 0, 0], align: 'right'  },
+        { width: 12 },
+        { width: 12 },
+    ]
+    
     ui.div({ text: chalk`{bold.yellow METAMORPHIC TEST RESULT}`, padding: [1, 1, 1, 1], align: 'center'})
     ui.div(
-        { text: chalk`{${headerColor} Test}`, ...firstColSetting }, 
-        { text: chalk`{${headerColor} Relations}`, },
-        { text: chalk`{${headerColor} Test Cases}`, ...thirdColSetting },
-        { text: chalk`{${headerColor} Failed}`, ...defaultColSetting },
-        { text: chalk`{${headerColor} Percentage}`, ...defaultColSetting }, 
+        { text: chalk`{${headerColor} Test}`, ...columnSetting[0] }, 
+        { text: chalk`{${headerColor} Relations}`, ...columnSetting[1] },
+        { text: chalk`{${headerColor} Test Cases}`, ...columnSetting[2] },
+        { text: chalk`{${headerColor} Failed}`, ...columnSetting[3] },
+        { text: chalk`{${headerColor} Percentage}`, ...columnSetting[4] }, 
     );
     contents.forEach((row, idx) => {
         const percentage = Number.parseFloat(row.passed/row.testCases*100).toFixed(2);
         const color = rowColor(row.failed);
         ui.div(
-            { text: chalk`{${color} ${idx+1}}`, ...firstColSetting }, 
-            { text: chalk`{${color} ${row.relation.description}}`, },
-            { text: chalk`{${color} ${row.testCases}}`, ...thirdColSetting },
-            { text: chalk`{${color} ${row.failed}}`, ...defaultColSetting },
-            { text: chalk`{${color} ${percentage}%}`, ...defaultColSetting }, 
+            { text: chalk`{${color} ${idx+1}}`, ...columnSetting[0] }, 
+            { text: chalk`{${color} ${row.relation.description}}`, ...columnSetting[1] },
+            { text: chalk`{${color} ${row.testCases}}`, ...columnSetting[2] },
+            { text: chalk`{${color} ${row.failed}}`, ...columnSetting[3] },
+            { text: chalk`{${color} ${percentage}%}`, ...columnSetting[4] }, 
         );
     });
+    const border = '‒'
+    ui.div(
+        { text: chalk`{${footerColor} ${border.repeat(columnSetting[0].width-1)}}`, ...columnSetting[0] },
+        { text: chalk`{${footerColor} }`, ...columnSetting[1] },
+        { text: chalk`{${footerColor} ${border.repeat(columnSetting[2].width-1)}}`, ...columnSetting[2] },
+        { text: chalk`{${footerColor} ${border.repeat(columnSetting[3].width-1)}}`, ...columnSetting[3] },
+        { text: chalk`{${footerColor} ${border.repeat(columnSetting[4].width-1)}}`, ...columnSetting[4] },
+    )
+    // ui.div(
+    //     { text: chalk`{${footerColor} ${contents.length}}`, ...columnSetting[0] },
+    //     { text: chalk`{${footerColor} }`, ...columnSetting[1] },
+    //     { text: chalk`{${footerColor} ${border.repeat(columnSetting[2].width-1)}}`, ...columnSetting[2] },
+    //     { text: chalk`{${footerColor} ${border.repeat(columnSetting[3].width-1)}}`, ...columnSetting[3] },
+    //     { text: chalk`{${footerColor} ${border.repeat(columnSetting[4].width-1)}}`, ...columnSetting[4] },
+    // )
     return ui.toString();
 }
 
@@ -47,11 +68,6 @@ function showSummary(summary) {
     } = summary;
 
     log(createResultSummaryTable(details));
-    log(chalk`
-        {yellow.bold.inverse SUMMARY}
-        {yellow Relations: {${failedRelations ? 'red' : 'green'} ${passedRelations}/${totalRelations}}}
-        {yellow Test Cases: {${failedTestCases ? 'red' : 'green'} ${passedTestCases}/${totalTestCases}}}
-    `)
 }
 
 function showAllResults(results) {
