96c8e3c684a0179f5aaf8a2fc8277e3dab123a15
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-04-17 10:32:19 +0700

    Removed unnecessary code.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// module.exports = {
-//     MetamorphicTesting: require('./src/core/MetamorphicTesting.js'),
-//     APIUnderTest: require('./src/core/APIUnderTest.js'),
-//     MetamorphicRelation: require('./src/core/MetamorphicRelation.js'),
-//     TestExecutor: require('./src/core/TestExecutor.js'),
-//     helper: require('./src/helper/helpers.js'),
-// };
-
 module.exports = require('./src/utils/syntax.js');
