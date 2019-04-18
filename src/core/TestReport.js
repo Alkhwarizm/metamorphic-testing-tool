534310963089f18e7b5e01b3965edfc4fa868eb4
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-04-18 09:27:36 +0700

    Refactored TestResult class.

--- src/core/TestReport.js
+++ src/core/TestReport.js
@@ -11,35 +11,36 @@ class TestRecord {
     }
 }
 
-function getTestSummary(result) {
-    const details = result.map((item, idx) => ({
-      relation: {
-        description: item.relation.description,
-        idx,
-      },
-      testCases: item.meta.total,
-      failed: item.meta.failed,
-      passed: item.meta.passed,
-    }));
-    const overview = {
-      totalRelations: result.length,
-      totalTestCases: result.reduce((acc, curr) => acc + curr.meta.total, 0),
-      failedRelations: result.filter(r => !r.meta.result).length,
-      failedTestCases: result.reduce((acc, curr) => acc + curr.meta.failed, 0),
-      passedRelations: result.filter(r => r.meta.result).length,
-      passedTestCases: result.reduce((acc, curr) => acc + curr.meta.passed, 0),
-    };
-    return {
-      overview,
-      details,
-    };
-  }
-
 class TestResults {
     constructor(records, targetAPI) {
         this.records = records;
         this.target = targetAPI;
-        this.summary = getTestSummary(records);
+        this.summary = this.calculateSummary();
+    }
+
+    calculateSummary() {
+        const result = this.records;
+        const details = result.map((item, idx) => ({
+            relation: {
+              description: item.relation.description,
+              idx,
+            },
+            testCases: item.meta.total,
+            failed: item.meta.failed,
+            passed: item.meta.passed,
+          }));
+          const overview = {
+            totalRelations: result.length,
+            totalTestCases: result.reduce((acc, curr) => acc + curr.meta.total, 0),
+            failedRelations: result.filter(r => !r.meta.result).length,
+            failedTestCases: result.reduce((acc, curr) => acc + curr.meta.failed, 0),
+            passedRelations: result.filter(r => r.meta.result).length,
+            passedTestCases: result.reduce((acc, curr) => acc + curr.meta.passed, 0),
+          };
+          return {
+            overview,
+            details,
+          };
     }
 }
 
